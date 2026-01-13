b9c25df54c723e9feaf7dc74f75d1d94593a3aba
Brian Wang <brian.wang900@gmail.com>
2026-01-14 00:03:37 +1100

    initial

---
+++ src/components/modules/hero/SVLJoinLandingHero.tsx
@@ -0,0 +1,31 @@
+import Module from "@/components/modules/Module";
+import Image from "next/image";
+
+export default function SVLJoinLandingHero() {
+  return (
+    <Module
+      className="py-0 min-h-[80vh] md:min-h-[85vh] relative"
+      backgroundImage="/WEBSITE MATERIAL/2026 SYRIO WEBSITE FILE_HIGHP2.png"
+      backgroundImageAlt="SYRIO REP Welcome"
+      backgroundImageClassName="object-cover"
+      contentClassName="hidden"
+      backgroundComponent={
+        <>
+          {/* Overlay */}
+          <div className="absolute inset-0 bg-syrio-black/70 z-10" />
+
+          {/* Icon - Fills entire module */}
+          <div className="absolute inset-0 z-20">
+            <Image
+              src="/WEBSITE MATERIAL/2026 SYRIO WEBSITE FILE_HIGHP2_ICON_HIGHP2.png"
+              alt="SYRIO REP Icon"
+              fill
+              className="object-contain"
+              priority
+            />
+          </div>
+        </>
+      }
+    />
+  );
+}
